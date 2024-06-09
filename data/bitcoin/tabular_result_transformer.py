77cdaa909c1e833c86c21d45d26a19ee715cbe5e
kamil <kamil.wojciechowski@chain-insights.ai>
2024-06-09 09:59:08 +0200

    reverted some changes

--- data/bitcoin/tabular_result_transformer.py
+++ data/bitcoin/tabular_result_transformer.py
@@ -3,7 +3,7 @@ def transform_result_set(result):
         return {"columns": [], "rows": []}
 
     # Dynamically generate columns based on the keys in the first result item
-    columns = [{"name": column_name } for column_name in result[0].keys()]
+    columns = [{"name": key, "label": key.title()} for key in result[0].keys()]
 
     # Populate the content based on the result set
     rows = [{**item} for item in result]
